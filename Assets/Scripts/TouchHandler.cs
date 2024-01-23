49e8d0f088d4f16529668ad3390808d0788f9a69
Jack-Patterson <jackpatterson.10.02@gmail.com>
2024-01-23 11:00:28 +0000

    Touches added

--- Assets/Scripts/TouchHandler.cs
+++ Assets/Scripts/TouchHandler.cs
@@ -5,104 +5,100 @@ using UnityEngine;
 
 namespace com.GamesForMobileDevices
 {
-    public class TouchHandler: MonoBehaviour
+    public class TouchHandler : MonoBehaviour
     {
-        private const float MaxTimeForSwipe = 0.5f;
-        private float _touchTimer;
-        private bool _hasMoved;
+        private const float MaxTimeForTap = 0.5f;
+        private Camera MainCamera => Camera.main;
+        private float _touchTimer = 0;
+        private bool _hasMoved = false;
         GestureAction _actOn;
-        private List<TouchHandler> _touchHandlers = new List<TouchHandler>();
         private GameObject _selectedObject;
-        private Camera _mainCamera;
 
         private void Start()
         {
-            _mainCamera = Camera.main;
             _actOn = FindObjectOfType<GestureAction>();
         }
-        
-        private void CheckTouch()
+
+        internal void CheckTouch(Touch touch)
         {
-            Touch[] currentTouches = Input.touches;
+            Vector3 touchPosition = touch.position;
 
-            if (currentTouches.Length >= 1)
+            switch (touch.phase)
             {
-                Touch touch = currentTouches[0];
-                Vector3 touchPosition = touch.position;
-                float distance = Vector3.Distance(touchPosition, _mainCamera.transform.position);
+                case TouchPhase.Began:
 
-                switch (touch.phase)
-                {
-                    case TouchPhase.Began:
-                        _touchTimer = 0f;
-                        _hasMoved = false;
-
-                        Ray ray = _mainCamera.ScreenPointToRay(touch.position);
-                        if (Physics.Raycast(ray, out RaycastHit hit))
+                    Ray ray = MainCamera.ScreenPointToRay(touch.position);
+                    if (Physics.Raycast(ray, out RaycastHit hit))
+                    {
+                        if (hit.collider.GetComponent<IInteractable>() != null)
                         {
-                            if (hit.collider.GetComponent<IInteractable>() != null)
-                            {
-                                _selectedObject = hit.collider.gameObject;
-                                _selectedObject.GetComponent<IInteractable>().EnableOutline();
-                                print("Distance: " + Vector3.Distance(_mainCamera.transform.position,
-                                    _selectedObject.transform.position));
-                            }
+                            _selectedObject = hit.collider.gameObject;
+                            _selectedObject.GetComponent<IInteractable>().EnableOutline();
+                            print("Distance: " + Vector3.Distance(MainCamera.transform.position,
+                                _selectedObject.transform.position));
                         }
+                    }
 
-                        break;
-                    case TouchPhase.Stationary:
-                        _touchTimer += Time.deltaTime;
-                        break;
-                    case TouchPhase.Moved:
-                        _hasMoved = true;
+                    break;
+                case TouchPhase.Stationary:
+                    _touchTimer += Time.deltaTime;
+                    break;
+                case TouchPhase.Moved:
+                    _hasMoved = true;
 
-                        if (_selectedObject != null)
-                        {
-                            Vector3 touchPos = new Vector3(touch.position.x, touch.position.y, 0);
-                            Ray rayC = _mainCamera.ScreenPointToRay(touchPos);
-                            
-                            float orbitDistance = Vector3.Distance(_mainCamera.transform.position, _selectedObject.transform.position);
-                            Vector3 newPosition = _mainCamera.transform.position + rayC.direction * orbitDistance;
-                            _selectedObject.transform.position = newPosition;
-                            
-                        }
-                        else
-                        {
-                            float rotateX = -touch.deltaPosition.y * 5f * Time.deltaTime;
-                            float rotateY = touch.deltaPosition.x * 5f * Time.deltaTime;
-                            
-                            Quaternion currentRotation = transform.rotation;
-                            Quaternion newRotation = Quaternion.Euler(
-                                currentRotation.eulerAngles.x + rotateX,
-                                currentRotation.eulerAngles.y + rotateY,
-                                0
-                            );
-                            transform.rotation = newRotation;
-                        }
+                    if (_selectedObject != null)
+                    {
+                        Vector3 touchPos = new Vector3(touch.position.x, touch.position.y, 0);
+                        Ray rayC = MainCamera.ScreenPointToRay(touchPos);
 
-                        break;
-                    case TouchPhase.Ended:
-                        _selectedObject?.GetComponent<IInteractable>()?.DisableOutline();
-                        _actOn.TapAt(touch.position);
+                        float orbitDistance = Vector3.Distance(MainCamera.transform.position,
+                            _selectedObject.transform.position);
+                        Vector3 newPosition = MainCamera.transform.position + rayC.direction * orbitDistance;
+                        _selectedObject.transform.position = newPosition;
+                    }
+                    else
+                    {
+                        float rotateX = -touch.deltaPosition.y * 5f * Time.deltaTime;
+                        float rotateY = touch.deltaPosition.x * 5f * Time.deltaTime;
 
-                        if (_touchTimer <= MaxTimeForSwipe && !_hasMoved)
-                        {
-                            print($"Tap at {touch.position}");
-                        }
-                        else if (_hasMoved)
-                        {
-                            print($"Swipe at {touch.position}");
-                        }
-                        else
-                        {
-                            print($"Hold at {touch.position}");
-                        }
+                        Quaternion currentRotation = transform.rotation;
+                        Quaternion newRotation = Quaternion.Euler(
+                            currentRotation.eulerAngles.x + rotateX,
+                            currentRotation.eulerAngles.y + rotateY,
+                            0
+                        );
+                        MainCamera.transform.rotation = newRotation;
+                    }
 
-                        _selectedObject = null;
+                    break;
+                case TouchPhase.Ended:
+                    _selectedObject?.GetComponent<IInteractable>()?.DisableOutline();
+                    _actOn.TapAt(touch.position);
 
-                        break;
-                }
+                    if (_touchTimer <= MaxTimeForTap && !_hasMoved)
+                    {
+                        print($"Tap at {touch.position}");
+                    }
+                    else if (_hasMoved)
+                    {
+                        print($"Swipe at {touch.position}");
+                    }
+                    else
+                    {
+                        print($"Hold at {touch.position}");
+                    }
+
+                    _selectedObject = null;
+
+                    break;
             }
+        }
+
+        internal void Reset()
+        {
+            _touchTimer = 0;
+            _hasMoved = false;
+            _selectedObject = null;
         }
     }
 }
--- Assets/Scripts/TouchManager.cs
+++ Assets/Scripts/TouchManager.cs
@@ -1,124 +1,71 @@
+#nullable enable
+
 using System.Collections.Generic;
-using com.GamesForMobileDevices.Interactable;
 using UnityEngine;
 
 namespace com.GamesForMobileDevices
 {
     public class TouchManager : MonoBehaviour
     {
-        private const float MaxTimeForSwipe = 0.5f;
-        private float _touchTimer;
-        private bool _hasMoved;
-        GestureAction _actOn;
-        private List<TouchHandler> _touchHandlers = new List<TouchHandler>();
-        private GameObject _selectedObject;
-        private Camera _mainCamera;
+        [SerializeField] private int maxTouches = 5;
+        private List<TouchHandler> _touchHandlers;
+        private List<Touch> _activeTouches;
 
         private void Start()
         {
-            _mainCamera = Camera.main;
-            _actOn = FindObjectOfType<GestureAction>();
+            InitializeTouchHandlers();
         }
 
         private void Update()
         {
-            CheckTouch();
-            AssignTouchHandler();
-
-            // print(selectedObject);
+            AssignTouches();
+            CheckTouches();
         }
 
-        private void AssignTouchHandler()
+        private void InitializeTouchHandlers()
         {
-            // Touch[] currentTouches = Input.touches;
-            //
-            // if (currentTouches.Length >= 1)
-            // {
-            //     if (_touchHandlers)
-            // }
-        }
+            _touchHandlers = new List<TouchHandler>(maxTouches);
+            _activeTouches = new List<Touch>(maxTouches);
 
-        private void CheckTouch()
-        {
-            Touch[] currentTouches = Input.touches;
-
-            if (currentTouches.Length >= 1)
+            for (int i = 0; i < maxTouches; i++)
             {
-                Touch touch = currentTouches[0];
-                Vector3 touchPosition = touch.position;
-                float distance = Vector3.Distance(touchPosition, _mainCamera.transform.position);
-
-                switch (touch.phase)
+                GameObject touchHandlerObject = new GameObject("TouchHandler_" + i);
+                touchHandlerObject.transform.parent = transform;
+                TouchHandler touchHandler = touchHandlerObject.AddComponent<TouchHandler>();
+                _touchHandlers.Add(touchHandler);
+                
+                _activeTouches.Add(default);
+            }
+        }
+    
+        private void AssignTouches()
+        {
+            for (int i = 0; i < maxTouches; i++)
+            {
+                if (i < Input.touchCount)
                 {
-                    case TouchPhase.Began:
-                        _touchTimer = 0f;
-                        _hasMoved = false;
-
-                        Ray ray = _mainCamera.ScreenPointToRay(touch.position);
-                        if (Physics.Raycast(ray, out RaycastHit hit))
-                        {
-                            if (hit.collider.GetComponent<IInteractable>() != null)
-                            {
-                                _selectedObject = hit.collider.gameObject;
-                                _selectedObject.GetComponent<IInteractable>().EnableOutline();
-                                print("Distance: " + Vector3.Distance(_mainCamera.transform.position,
-                                    _selectedObject.transform.position));
-                            }
-                        }
-
-                        break;
-                    case TouchPhase.Stationary:
-                        _touchTimer += Time.deltaTime;
-                        break;
-                    case TouchPhase.Moved:
-                        _hasMoved = true;
-
-                        if (_selectedObject != null)
-                        {
-                            Vector3 touchPos = new Vector3(touch.position.x, touch.position.y, 0);
-                            Ray rayC = _mainCamera.ScreenPointToRay(touchPos);
-                            
-                            float orbitDistance = Vector3.Distance(_mainCamera.transform.position, _selectedObject.transform.position);
-                            Vector3 newPosition = _mainCamera.transform.position + rayC.direction * orbitDistance;
-                            _selectedObject.transform.position = newPosition;
-                            
-                        }
-                        else
-                        {
-                            float rotateX = -touch.deltaPosition.y * 5f * Time.deltaTime;
-                            float rotateY = touch.deltaPosition.x * 5f * Time.deltaTime;
-                            
-                            Quaternion currentRotation = transform.rotation;
-                            Quaternion newRotation = Quaternion.Euler(
-                                currentRotation.eulerAngles.x + rotateX,
-                                currentRotation.eulerAngles.y + rotateY,
-                                0
-                            );
-                            transform.rotation = newRotation;
-                        }
-
-                        break;
-                    case TouchPhase.Ended:
-                        _selectedObject?.GetComponent<IInteractable>()?.DisableOutline();
-                        _actOn.TapAt(touch.position);
-
-                        if (_touchTimer <= MaxTimeForSwipe && !_hasMoved)
-                        {
-                            print($"Tap at {touch.position}");
-                        }
-                        else if (_hasMoved)
-                        {
-                            print($"Swipe at {touch.position}");
-                        }
-                        else
-                        {
-                            print($"Hold at {touch.position}");
-                        }
-
-                        _selectedObject = null;
-
-                        break;
+                    _activeTouches[i] = Input.GetTouch(i);
                 }
+                else
+                {
+                    _activeTouches[i] = default;
+                }
+            }
+        }
+
+        private void CheckTouches()
+        {
+            for (int i = 0; i < maxTouches; i++)
+            {
+                _touchHandlers[i].CheckTouch(_activeTouches[i]);
+
+                if (_activeTouches[i].phase != TouchPhase.Ended) continue;
+                _activeTouches[i] = default;
+
+                TouchHandler touchHandler = _touchHandlers[i];
+                _touchHandlers.Remove(touchHandler);
+                _touchHandlers.Add(touchHandler);
+                touchHandler.Reset();
             }
         }
     }
